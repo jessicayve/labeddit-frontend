f65b5a4874e361f9cf25f25fab4a439b0ac3acfc
jessicayve <yve.jessica@gmail.com>
2023-03-20 17:11:23 -0300

    página de login e sign up finalizados

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import { useState } from "react";
-import { GlobalContext } from "./context/GlobalContext";
-import Router from "./routes/Router";
+import React, { useEffect, useState } from 'react'
+import { GlobalContext } from "./context/GlobalContext"
+import { ChakraProvider } from '@chakra-ui/react'
+import Router from "./routes/Router"
 
 
 
 function App() {
   
+  const [ isAuth, setIsAuth ] = useState(false)
 
+    useEffect(() => {
+        const token = window.localStorage.getItem("labeddit-token")
+
+        if (token) {
+            setIsAuth(true)
+        }
+    }, [])
+
+    const context = {
+        isAuth: isAuth,
+        setIsAuth: setIsAuth
+    }
 
   return (
    
-      <>
-      <Router/>
-      </>
+    <GlobalContext.Provider value={context}>
+    <ChakraProvider>
+        <Router />
+    </ChakraProvider>
+</GlobalContext.Provider>
       
      
    
--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,24 +1,61 @@
 import { Input, Button, TextoContainer, Titulo } from "../SignUpPage/SignUpPageStyled"
 import  Header  from "../../components/Header"
-import { useState } from "react"
+import { useEffect, useState, useContext } from 'react'
+import { useNavigate } from "react-router"
+import axios from "axios"
+import { BASE_URL } from "../../constants/url"
+import { goToFeedPage } from "../../routes/coordinator"
+import CircularProgress from '@mui/material/CircularProgress'
+import { GlobalContext } from '../../context/GlobalContext'
 
 const SignupPage = () => {
 
+
+  const context = useContext(GlobalContext)
+  const navigate = useNavigate()
+
+  const [isLoading, setIsLoading] = useState(false)
+
   const [form, setForm] = useState({
-    apelido: "",
-    email: "",
-    password: "",
-  });
+      name: "",
+      email: "",
+      password: ""
+  })
 
+  // useEffect(() => {
+  //     if (context.isAuth) {
+  //         goToFeedPage(navigate)
+  //     }
+  // })
 
-  const changeForm = (event) => {
-    setForm({ ...form, [event.target.name]: event.target.value });
+  const onChangeForm = (event) => {
+      setForm({ ...form, [event.target.name]: event.target.value })
   }
 
+  const signup = async () => {
+      try {
+          setIsLoading(true)
 
-  const signup = () =>{
-    console.log(form)
-    
+          const body = {
+              name: form.name,
+              email: form.email,
+              password: form.password
+          }
+
+          const response = await axios.post(
+              `${BASE_URL}/users/signup`,
+              body
+          )
+
+          window.localStorage.setItem("tabeddit-token", response.data.token)
+          setIsLoading(false)
+          context.setIsAuth(true)
+
+          goToFeedPage(navigate)
+      } catch (error) {
+          console.log(error)
+          setIsLoading(false)
+      }
   }
 
   return(
@@ -30,22 +67,21 @@ Olá, boas vindas ao LabEddit ;)
 </Titulo> 
 
     <Input 
-    value={form.apelido} 
-    name={"apelido"} 
-    onChange={changeForm}  
+    value={form.name} 
+    name={"name"} 
+    onChange={onChangeForm}  
     placeholder="Apelido" />
 
     <Input 
     value={form.email}  
     name={"email"} 
-    onChange={changeForm} 
+    onChange={onChangeForm} 
     placeholder="E-mail"/>
 
     <Input 
     name={"password"}
     value={form.password}  
-    onChange={changeForm} 
-
+    onChange={onChangeForm} 
     type={"password"} 
     placeholder="Senha"/>
 
@@ -58,7 +94,7 @@ Olá, boas vindas ao LabEddit ;)
  </div>
 </TextoContainer>
 
-    <Button onClick={()=>signup()} >Cadastrar</Button>
+    <Button onClick={()=>signup()} > {isLoading ?  <CircularProgress color="inherit"  /> : "Cadastrar"}</Button>
 
 </>
 )
