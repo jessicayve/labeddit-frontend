04fd0f3108f53f76b4951da8a148f16531f745c0
jessicayve <yve.jessica@gmail.com>
2023-03-21 17:08:40 -0300

    deploy

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -3,14 +3,13 @@ import React from 'react'
 import FeedPage from "../pages/FeedPage/FeedPage"
 import SignUpPage from "../pages/SignUpPage/SignUpPage"
 import LoginPage from "../pages/LoginPage/LoginPage"
-import DetailsPage from "../pages/DetailsPage/DetailsPage";
+
 
 const Router = () => {
     return (
       <BrowserRouter>
       <Routes>
           <Route path="/feed" element={<FeedPage/>}/>
-          <Route path="/detailspage/:id" element={<DetailsPage/>}/>
           <Route path="/signup" element={<SignUpPage/>}/>
           <Route path="/" element={<LoginPage/>}/>
           
